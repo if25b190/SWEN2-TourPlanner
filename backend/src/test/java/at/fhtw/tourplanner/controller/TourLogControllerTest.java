3a5df8580d0d5797a1712de36ab0e22350c73dfe
= <=>
2026-04-30 16:07:24 +0200

    add tests

--- backend/src/test/java/at/fhtw/tourplanner/controller/TourLogControllerTest.java
+++ backend/src/test/java/at/fhtw/tourplanner/controller/TourLogControllerTest.java
@@ -1,4 +1,39 @@
 package at.fhtw.tourplanner.controller;
 
+import at.fhtw.tourplanner.TourplannerApplicationTests;
+import at.fhtw.tourplanner.service.TourLogService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = TourplannerApplicationTests.UserMockConfig.class
+)
+@AutoConfigureMockMvc
 public class TourLogControllerTest {
+    private static final String API_URL = "http://localhost:8080/api/v1";
+
+    @MockitoBean
+    private TourLogService  tourLogService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetTourLogs_whenNotLoggedIn() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get(API_URL + "/tours")
+                )
+                .andExpect(status().isUnauthorized());
+    }
 }
